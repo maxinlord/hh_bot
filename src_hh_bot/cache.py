4c913e888c978c37d5af0467ef487a6a8fc97256
Max <65973902+maxinlord@users.noreply.github.com>
2024-07-12 00:43:26 +0300

    ch

--- src_hh_bot/cache.py
+++ src_hh_bot/cache.py
@@ -1,4 +1,4 @@
 from cachetools import cached, TTLCache
 
-text_cache = TTLCache(maxsize=200, ttl=1000)
-button_cache = TTLCache(maxsize=100, ttl=1500)
+text_cache = TTLCache(maxsize=200, ttl=5)
+button_cache = TTLCache(maxsize=100, ttl=5)
